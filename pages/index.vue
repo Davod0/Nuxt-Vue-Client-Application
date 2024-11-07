184c5259614251ac20444ed003e2d70f94519d3d
Davod0 <davodnikoyii@gmai.com>
2024-11-07 10:04:01 +0100

    Created some new components, SmallCard, SmallCardDisplay, Nav

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,32 @@
 <template>
-  <div class="container">
-    <Hero />
-    <LargCardDisplay
-    v-for="cardInfo in largeCardInfo"
-    :key="cardInfo.id"
-    :cardsSection="cardInfo"
-    />
+  <div>
+    <Nav></Nav>
+    <div class="container">
+      <Hero />
+      <LargCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardSections"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import {largeCardSections} from "../assets/data.js"
+import { largeCardSections, smallCardSections } from "../assets/data.js";
+import Nav from "../components/Nav.vue";
+import SmallCardDisplay from "../components/SmallCardDisplay.vue";
 export default {
   data() {
     return {
-      largeCardInfo: largeCardSections
-    }
-  }
-}
+      largeCardInfo: largeCardSections,
+      smallCardSections
+    };
+  },
+};
 </script>
