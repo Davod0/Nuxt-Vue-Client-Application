9028969d7eb6f9afd0aca0c30dfb3c860e5cf06e
Davod0 <davodnikoyii@gmai.com>
2024-11-08 13:14:02 +0100

    Added an external library for a calendar

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,9 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/vue-calendar.js', mode: 'client' }
   ],
+// '~plugins/vue-calendar.js'
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
