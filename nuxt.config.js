f775e45b29ab48c5fe1fefd2b89304bbfdc79c01
Davod <davodnikoyii@gmail.com>
2025-10-29 13:46:20 +0100

    Remove unused Vercel configuration and clean up package.json scripts

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,6 @@
 export default {
   target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Nuxt-Vue-Client-Application',
     htmlAttrs: {
@@ -18,29 +17,22 @@ export default {
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/vue-calendar.js', mode: 'client' }
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
   ],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
 }
